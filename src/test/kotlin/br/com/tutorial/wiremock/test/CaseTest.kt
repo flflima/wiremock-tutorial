593132ed1812a5ba9324f7147ecedf578b9c2e5d
Felipe Lima <flf.felipe@gmail.com>
2019-06-23 17:44:48 -0300

    Adding tests with scenarios

--- src/test/kotlin/br/com/tutorial/wiremock/test/CaseTest.kt
+++ src/test/kotlin/br/com/tutorial/wiremock/test/CaseTest.kt
@@ -3,7 +3,10 @@ package br.com.tutorial.wiremock.test
 import com.github.kittinunf.fuel.httpGet
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import com.github.tomakehurst.wiremock.junit.WireMockRule
+import com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED
 import org.apache.http.HttpStatus
+import org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR
+import org.apache.http.HttpStatus.SC_OK
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -23,10 +26,26 @@ class CaseTest {
     fun testing() {
         stubFor(
             get(urlEqualTo("/my/resource"))
+                .inScenario("To do list")
+                .whenScenarioStateIs(STARTED)
+                .willSetStateTo("success")
                 .withHeader("Accept", equalTo("text/xml"))
                 .willReturn(
                     aResponse()
-                        .withStatus(200)
+                        .withStatus(SC_INTERNAL_SERVER_ERROR)
+                        .withBody("<response>ERROR</response>")
+                )
+        )
+
+
+        stubFor(
+            get(urlEqualTo("/my/resource"))
+                .inScenario("To do list")
+                .whenScenarioStateIs("success")
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(
+                    aResponse()
+                        .withStatus(SC_OK)
                         .withHeader("Content-Type", "text/xml")
                         .withBody("<response>Some content</response>")
                 )
@@ -39,9 +58,20 @@ class CaseTest {
             .header(mapOf("Accept" to "text/xml"))
             .response()
 
+
         println(String(response.data))
 
-        assertEquals(HttpStatus.SC_OK, response.statusCode)
+        assertEquals(SC_INTERNAL_SERVER_ERROR, response.statusCode)
+
+        val (_, response2, _) = "http://localhost:8089/my/resource"
+            .httpGet()
+            .header(mapOf("Accept" to "text/xml"))
+            .response()
+
+
+        println(String(response2.data))
+
+        assertEquals(SC_OK, response2.statusCode)
 
         verify(
             getRequestedFor(urlMatching("/my/resource"))
